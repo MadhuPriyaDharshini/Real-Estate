6ba23588cfa8c9ee56fb52908876e538a9648a7b
MadhuPriyaDharshini <madhupriya@student.tce.edu>
2023-01-31 14:50:15 +0530

    modified:   profile.html 	modified:   profile.js

--- profile.js
+++ profile.js
@@ -1,11 +1,27 @@
+function phonenumber(inputtxt)
+{
+  var phoneno = /^\d{10}$/;
 
   if(inputtxt.value.match(phoneno))
   {
-      flag = 1;
+      return true;
   }
   else
   {
      alert("Not a valid Phone Number");
      return false;
+  }
+  }
+
+  function validateForm(name)
+  {
+   const name1 = ""
+    if(name.value=="")
+     {alert("Oops!You've forgot to fill your name");
+      return false;
+     }
+    else{
+      return true;
+    }
   }
   
