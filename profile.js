d78848f280332945e90a21e5271478807a2fcaff
Lokesh <lokeshtrt0108@gmail.com>
2023-02-05 23:44:07 +0530

    .

--- profile.js
+++ profile.js
@@ -8,4 +8,21 @@ function phonenumber(inputtxt) {
     alert("Not a valid Phone Number");
     return false;
   }
+}
+
+document.getElementById("date1").addEventListener("change", function(){
+  var date = this.value;
+  var enteredDate = new Date(date);
+  console.log(enteredDate);
+  var startDate = new Date("1992-01-01");
+  var endDate = new Date("2022-03-04");
+  if (enteredDate < startDate || enteredDate > endDate) {
+    alert("Enter valid Date");
+    this.value = ""
   }
+});
+
+
+document.getElementById("password").addEventListener("input",function(){
+   
+})
